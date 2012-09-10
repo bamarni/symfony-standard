317ce94dd7db092e24ac5ebf96e5eefb79c37497
Bilal Amarni <bilal.amarni@gmail.com>
2012-09-10 15:27:10 +0200

    added a failing test

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -3,6 +3,8 @@
 namespace Acme\DemoBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DemoControllerTest extends WebTestCase
 {
@@ -13,5 +15,20 @@ class DemoControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/demo/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+
+    public function testFailure()
+    {
+        $client = static::createClient();
+        $container = static::$kernel->getContainer();
+
+        $client->getCookieJar()->set(new Cookie($container->get('session')->getName(), true));
+        $token = new UsernamePasswordToken('admin', null, 'secured_area', array('ROLE_ADMIN'));
+        $container->get('security.context')->setToken($token);
+        $container->get('session')->set('_security_secured_area', serialize($token));
+
+        $client->request('GET', '/demo/secured/hello/Fabien');
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
